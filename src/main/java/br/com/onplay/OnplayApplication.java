e9f61fad92f330bd4ea397ea91c1a66caaa73ca9
Eduardo Caetano <eduardocaetano110@gmail.com>
2025-12-02 10:05:26 -0300

    good first commit

---
+++ src/main/java/br/com/onplay/OnplayApplication.java
@@ -0,0 +1,13 @@
+package br.com.onplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnplayApplication.class, args);
+    }
+
+}
